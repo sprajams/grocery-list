7fed561b3bd76681ac8ad11ab5c377af68220e41
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-05-24 18:02:17 -0700

    styling of suggestion list

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -32,34 +32,44 @@ function Form({ setList }) {
     <div className={styles.out}>
       <h1>My shopping list:</h1>
 
-      <form className={styles.form}>
-        <input
-          type="text"
-          id="name"
-          value={ingredient}
-          onChange={handleChange}
-          className={styles.input}
-        ></input>
-        <button onClick={addItem}>Add</button>
-      </form>
+      <div className={styles.wrap}>
+        <form className={styles.form}>
+          <input
+            type="text"
+            id="name"
+            value={ingredient}
+            onChange={handleChange}
+            className={styles.input}
+          ></input>
+          <button onClick={addItem} className={styles.addBtn}>
+            Add
+          </button>
+        </form>
 
-      <ul className={styles.suggestionList}>
-        {data.map((x, i) => {
-          //clicking on a suggestion adds item to the shopping list, clears out input & other suggestions
-          const onClick = () => {
-            setList((oldArray) => [...oldArray, x]);
-            setData([]);
-            setIngredient("");
-          };
-          return (
-            <li key={i}>
-              <button className={styles.suggestion} onClick={onClick}>
-                {x}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+        <ul
+          className={
+            data.length > 1
+              ? `${styles.suggestionList} ${styles.suggestionListOutlined}`
+              : `${styles.suggestionList}`
+          }
+        >
+          {data.map((x, i) => {
+            //clicking on a suggestion adds item to the shopping list, clears out input & other suggestions
+            const onClick = () => {
+              setList((oldArray) => [...oldArray, x]);
+              setData([]);
+              setIngredient("");
+            };
+            return (
+              <li key={i}>
+                <button className={styles.suggestion} onClick={onClick}>
+                  {x}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
